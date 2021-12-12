abfb481b88f31122aeb0f0c244879ea8717c0d25
Hektor-Byfors <hektor.byfors@hotmail.com>
2021-12-12 17:52:12 +0100

    localStorage done

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let logoutbutton = document.querySelector(".logoutButton");
 let inputField = document.querySelector(".inputField");
 let welcomeText = document.querySelector("#welcomeText");
 let wrongInput = document.querySelector("#wrongInput");
+let storedInput = localStorage.getItem("username:");
 
 let contentPara = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
@@ -26,16 +27,18 @@ let users = [
 ];
 
 const login = () => {
-    let loggedIn = false;
-    
     for(let i = 0; i < users.length; i++){
         if(username.value == users[i].name && password.value == users[i].pass){
-            loggedIn = true;
-            welcomeText.innerHTML = `Welcome ${users[i].name}`;
+            localStorage.setItem("username:", users[i].name);
+        }else{
+            wrongInput.classList.remove("hide");
+            wrongInput.classList.add("show");
         }
     }
 
-    if(loggedIn){
+    storedInput = localStorage.getItem("username:");
+    
+    if(storedInput){
         mainPara.innerHTML = contentPara;
         username.classList.add("hide");
         password.classList.add("hide");
@@ -47,10 +50,23 @@ const login = () => {
         button.classList.remove("show");
         logoutbutton.classList.remove("hide");
         logoutbutton.classList.add("show");
-    }else{
-        wrongInput.classList.remove("hide");
-        wrongInput.classList.add("show");
+        welcomeText.innerHTML = `Welcome ${storedInput}`;
     }
+}
+
+if(storedInput){
+    mainPara.innerHTML = contentPara;
+    username.classList.add("hide");
+    password.classList.add("hide");
+    welcomeText.classList.remove("hide");
+    welcomeText.classList.add("show");
+    wrongInput.classList.add("hide");
+    wrongInput.classList.remove("show");
+    button.classList.add("hide");
+    button.classList.remove("show");
+    logoutbutton.classList.remove("hide");
+    logoutbutton.classList.add("show");
+    welcomeText.innerHTML = `Welcome ${storedInput}`;
 }
 
 const logout = () => {
@@ -63,6 +79,7 @@ const logout = () => {
     button.classList.add("show");
     logoutbutton.classList.add("hide");
     logoutbutton.classList.remove("show");
+    localStorage.removeItem("username:");
 }
 
 button.addEventListener("click", login);
